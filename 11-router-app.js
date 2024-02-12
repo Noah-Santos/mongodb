ed749c65ea281b4c47ea56415c060405c6487ddc
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-02-12 13:14:00 -0700

    Base Files
    Made the base files for the project

---
+++ 11-router-app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+require('dotenv').config();
+require('./db/connect');
+
+const people = require('./routes/people-controller');
+const connectDB = require('./db/connect');
+
+// static assets
+app.use(express.static('./public'));
+// parse from data
+app.use(express.urlencoded({ extended: false }));
+// parse JSON data
+app.use(express.json());
+
+// routes/router
+app.use('/api/people', people);
+
+// Server Listen
+const initServer = async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(5000, ()=>{
+            console.log('Server listening on port 5000')
+        });
+    }catch(e){
+        console.log(e);
+    }
+}
+
+initServer();
---
+++ models/person.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const PersonSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'Must provide a name'],
+        trim: true,
+    },
+    task:{
+        type: String,
+        trim: true,
+        default: 'none'
+    },
+    age:{
+        type: Number,
+        default: 10
+    },
+    userID:{
+        type: Number,
+        default: 0
+    }
+},{collection: 'People'})
+
+module.exports = mongoose.model('Person', PersonSchema);
