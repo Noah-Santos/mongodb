82cd6f33cab6cd2ebe7201c8a49af55c77dd686c
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-02-22 15:39:14 -0700

    Routines
    The dropdown routines now sync with MongoDB. Creating the form to add a routine and working on the delete routine

--- public/access.js
+++ public/access.js
@@ -1,9 +1,11 @@
+// denies users access into the sample application if they do not sign in
 let signedIn = sessionStorage.getItem('signedIn') || false;
 console.log(signedIn);
 if(signedIn){
     window.location.href = './sample.html';
 }
 
+// gets the users from the MongoDB database
 const fetchUsers = async()=>{
     try {
         const data = await fetch('/people').then(response=>{
@@ -15,16 +17,16 @@ const fetchUsers = async()=>{
     }
 }
 
+// checks to see if the user credentials match a user in the database
 async function login(){
     let users = await fetchUsers();
-    console.log(users);
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
 
     users.map(user=>{
         if(user.email == email || user.password == password){
             sessionStorage.setItem('signedIn', true);
-            sessionStorage.setItem('user', user);
+            sessionStorage.setItem('currentUser', JSON.stringify(user));
             window.location.href = './sample.html';
         }
     })
--- public/sample.js
+++ public/sample.js
@@ -1,5 +1,66 @@
+// kicks user to the sign in page if they try to access the sample application without signing in
 let signedIn = sessionStorage.getItem('signedIn') || false;
-console.log(signedIn)
 if(!signedIn){
     window.location.href = './login.html';
+}
+let currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+console.log(currentUser);
+let routineSection = document.querySelector('#routine');
+let exerciseSection = document.getElementById('exerciseSection');
+
+// const fetchUsers = async()=>{
+//     try {
+//         const data = await fetch('/people').then(response=>{
+//             return response.json();
+//         });
+//         return data;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// function to create a new routine
+async function createRoutine(){
+    let routineName = document.getElementById('routineName');
+    let newRoutine = {
+        name:routineName,
+        exercises: [],
+    }
+    let routines = currentUser.routines;
+    routines.push(newRoutine);
+    fetch(`/people/${currentUser.userID}`, {
+        method: "PUT",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({routines:routines}),
+    })
+}
+
+// function to display the routines
+function showRoutines(){
+    routineSection.innerHTML = '';
+    console.log(currentUser)
+    console.log(JSON.parse(sessionStorage.getItem('currentUser')))
+    let routines = currentUser.routines.map((routine, index)=>{
+        return `
+            <option value=${index}>${routine.name}</option>
+        `
+    });
+    routineSection.innerHTML = routines.join('');
+
+}
+showRoutines();
+
+// function to delete a routine
+async function deleteRoutine(){
+    
+}
+
+// function to create a new exercise
+async function createExercise(){
+
+}
+
+// function to display the exercises
+async function showExercises(){
+
 }
